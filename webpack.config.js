fdb5f9e8530737ef3476a9c44c6d447497d9414d
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-27 10:47:02 -0700

    Run NPM build add API webpack.
    Co-authored-by: Joseph Jack <joseph.w.jack@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,9 +14,10 @@ module.exports = {
     contentBase: './dist'
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Weather',
       template: './src/index.html',
       inject: 'body'
     })
